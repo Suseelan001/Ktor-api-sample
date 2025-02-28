5a70db557bc8170992d829c5643871e6a65bdba2
suseelan G <suseelan13203@gmail.com>
2025-02-28 15:43:20 +0530

    creating api

--- ktor-server-db-intagration/src/main/kotlin/Application.kt
+++ ktor-server-db-intagration/src/main/kotlin/Application.kt
@@ -1,9 +1,14 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
+    embeddedServer(Netty, port) {
+        module()
+    }.start(wait = true)
 }
 
 fun Application.module() {
