1bb7a4600aad48824c3b75c8346ad636036c90d7
suseelan G <suseelan13203@gmail.com>
2025-03-14 11:06:03 +0530

    docker installation

--- ktor-server-db-intagration/src/main/kotlin/Application.kt
+++ ktor-server-db-intagration/src/main/kotlin/Application.kt
@@ -3,16 +3,27 @@ package com.example
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.util.logging.Logger
+
+private val logger = Logger.getLogger("ktor")
 
 fun main() {
     val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
-    embeddedServer(Netty, port) {
-        module()
-    }.start(wait = true)
+    try {
+        embeddedServer(Netty, port) {
+            module()
+        }.start(wait = true)
+    } catch (e: Exception) {
+        logger.severe("Failed to start server: $e")
+    }
 }
 
 fun Application.module() {
-    configureSerialization()
-    configureDatabases()
-    configureRouting()
+    try {
+        configureSerialization()
+        configureDatabases()
+        configureRouting()
+    } catch (e: Exception) {
+        logger.severe("Failed to configure application: $e")
+    }
 }
